70fd212e4af86266d21787634a6961e8be23bf44
Chai3517 <chaizhihang3@gmail.com>
2025-04-26 18:41:09 +0100

    Add files via upload

---
+++ Assets/Scripts/AI_CardEffect/AICardEffectInsane4.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+
+public class AICardEffectInsane4 : MonoBehaviour, IInsaneCard
+{
+    public void ExecuteSaneEffect(Player currentPlayer)
+    {
+        Debug.Log("������Ч�����㽫���»غ�ǰ������������Ч��");
+
+        currentPlayer.SetProtected(true); // ���ñ���
+        currentPlayer.GoInsane();         // ���Ǵ�����Ϊ���
+
+        // ����˵����壬ȷ�Ϻ�����غ�
+        var noticeUI = Object.FindFirstObjectByType<UICardEffectNotice>();
+        if (noticeUI != null)
+        {
+            if (currentPlayer.isHuman == true)
+            {
+                noticeUI.Show(
+                    "You cannot be chosen as part of the effects of other players' cards until the start of your next turn.",
+                    () => GameManager.Instance.EndTurn()
+                );
+            }
+            else
+            {
+                Debug.LogWarning("�Ҳ��� UICardEffectNotice��ֱ�ӽ����غ�");
+                GameManager.Instance.EndTurn();
+            }
+        }
+    }
+
+    public void ExecuteInsaneEffect(Player currentPlayer)
+    {
+        Debug.Log("�����Ч�����㽫�ڱ�����Ϸ����ǰ���ᱻ��̭");
+
+        currentPlayer.SetImmortalThisRound(true); // ���ò���
+
+        var noticeUI = Object.FindFirstObjectByType<UICardEffectNotice>();
+        if (noticeUI != null)
+        {
+            if (currentPlayer.isHuman == true)
+            {
+                noticeUI.Show(
+                    "You will not be knocked out until the end of this round.",
+                    () => GameManager.Instance.EndTurn()
+                );
+            }
+            else
+            {
+                Debug.LogWarning("�Ҳ��� UICardEffectNotice��ֱ�ӽ����غ�");
+                GameManager.Instance.EndTurn();
+            }
+        }
+    }
+}
