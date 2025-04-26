3525f90df10e9ed4313e95b3b004dab8752152aa
CarlCGG <155146997+CarlCGG@users.noreply.github.com>
2025-04-26 19:30:44 +0100

    Add files via upload

---
+++ Assets/Scripts/AI_CardEffect/AiCardEffect3.cs
@@ -0,0 +1,74 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class AiCardEffect3 : MonoBehaviour, IMainEffect
+{
+    public void ExecuteEffect(Player currentPlayer)
+    {
+        List<Player> targets = GameManager.Instance.GetAvailableTargets(currentPlayer);
+
+        if (targets.Count == 0)
+        {
+            UIManager.Instance.ShowPopup("无法选择");
+             // 获取要弃掉的卡
+            Card cardToDiscard = currentPlayer.GetSelectedCard(); 
+            if (cardToDiscard != null)
+            {
+                currentPlayer.DiscardCard(cardToDiscard);
+            }
+
+            GameManager.Instance.EndTurn();
+            return;
+        }
+
+        if(currentPlayer.isHuman = true){
+        UIManager.Instance.ShowPlayerSelectionSimple(targets, selectedTarget =>
+        {
+            int currentValue = currentPlayer.GetHandValue();
+            int targetValue = selectedTarget.GetHandValue();
+
+            UIManager.Instance.Log($"{currentPlayer.playerName} 的手牌是 {currentValue}");
+            UIManager.Instance.Log($"{selectedTarget.playerName} 的手牌是 {targetValue}");
+
+            if (currentValue > targetValue)
+            {
+                selectedTarget.Eliminate();
+                UIManager.Instance.ShowPopup($"{currentPlayer.playerName} 赢了！{selectedTarget.playerName} 出局！");
+            }
+            else if (currentValue < targetValue)
+            {
+                currentPlayer.Eliminate();
+                UIManager.Instance.ShowPopup($"{selectedTarget.playerName} 赢了！{currentPlayer.playerName} 出局！");
+            }
+            else
+            {
+                UIManager.Instance.ShowPopup("平局！没人出局");
+            }
+
+            GameManager.Instance.EndTurn();
+        });
+    }else{
+            int randomIndex = UnityEngine.Random.Range(0,targets.Count);
+            int currentValue = currentPlayer.GetHandValue();
+            int targetValue = targets[randomIndex].GetHandValue();
+
+            if (currentValue > targetValue)
+            {
+                targets[randomIndex].Eliminate();
+                UIManager.Instance.ShowPopup($"{currentPlayer.playerName} Ӯ�ˣ�{targets[randomIndex].playerName} ���֣�");
+            }
+            else if (currentValue < targetValue)
+            {
+                currentPlayer.Eliminate();
+                UIManager.Instance.ShowPopup($"{targets[randomIndex].playerName} Ӯ�ˣ�{currentPlayer.playerName} ���֣�");
+            }
+            else
+            {
+                UIManager.Instance.ShowPopup("ƽ�֣�û�˳���");
+            }
+
+            GameManager.Instance.EndTurn();
+
+    };
+    }
+}
---
+++ Assets/Scripts/AI_CardEffect/AiCardEffect5.cs
@@ -0,0 +1,70 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class AiCardEffect5 : MonoBehaviour, IMainEffect
+{
+    public void ExecuteEffect(Player currentPlayer)
+    {
+        // 包括自己，且活着、未受保护
+        List<Player> targetPlayers = GameManager.Instance.players.FindAll(
+            p => p.IsAlive() && !p.IsProtected()
+        );
+
+        if (targetPlayers.Count == 0)
+        {   
+            UIManager.Instance.ShowPopup("无法选择");
+
+            // 获取要弃掉的卡
+            Card cardToDiscard = currentPlayer.GetSelectedCard(); 
+            if (cardToDiscard != null)
+            {
+                currentPlayer.DiscardCard(cardToDiscard);
+            }
+
+            GameManager.Instance.EndTurn();
+            return;
+        }
+
+        if(currentPlayer.isHuman == true){
+        UIManager.Instance.ShowPlayerSelectionAllowSelf(targetPlayers, selectedTarget =>
+        {
+            Card oldCard = selectedTarget.RemoveCard();
+
+            if (oldCard != null)
+            {
+                selectedTarget.DiscardCard(oldCard);
+                Debug.Log($"{selectedTarget.playerName} 弃掉了 {oldCard.cardName}");
+            }
+            else
+            {
+                Debug.Log($"{selectedTarget.playerName} 没有手牌可以弃掉");
+            }
+
+            selectedTarget.DrawCard(GameManager.Instance.deck);
+
+            UIManager.Instance.ShowPopup($"{selectedTarget.playerName} 弃掉了手牌并抽了一张新牌");
+            GameManager.Instance.EndTurn();
+        });
+    }else{
+            int randomIndex = UnityEngine.Random.Range(0,targetPlayers.Count);
+        
+    
+            Card oldCard = targetPlayers[randomIndex].RemoveCard();
+
+            if (oldCard != null)
+            {
+                targetPlayers[randomIndex].DiscardCard(oldCard);
+                Debug.Log($"{targetPlayers[randomIndex].playerName} 弃掉了 {oldCard.cardName}");
+            }
+            else
+            {
+                Debug.Log($"{targetPlayers[randomIndex].playerName} 没有手牌可以弃掉");
+            }
+
+            targetPlayers[randomIndex].DrawCard(GameManager.Instance.deck);
+
+            UIManager.Instance.ShowPopup($"{targetPlayers[randomIndex].playerName} 弃掉了手牌并抽了一张新牌");
+            GameManager.Instance.EndTurn();
+    };
+    }
+}
