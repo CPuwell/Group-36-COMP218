9a7471907e5e138f509bad074899ebe8065778e5
CPuwell <254088979@qq.com>
2025-04-27 14:34:40 +0100

    test
    test is it a usable commit

--- Assets/Scripts/AI_CardEffect/AiCardEffect2.cs
+++ Assets/Scripts/AI_CardEffect/AiCardEffect2.cs
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 using UnityEngine;
 using System.Collections.Generic;
 
@@ -52,58 +52,3 @@ public class AiCardEffect2 : MonoBehaviour, IMainEffect
         ;
     }
 }
-=======
-using UnityEngine;
-using System.Collections.Generic;
-
-public class AiCardEffect2 : MonoBehaviour, IMainEffect
-{
-    public void ExecuteEffect(Player currentPlayer)
-    {
-        List<Player> targetPlayers = GameManager.Instance.GetAvailableTargets(currentPlayer);
-
-        if (targetPlayers.Count == 0)
-        {
-            UIManager.Instance.ShowPopup("没有其他玩家可供查看");
-             // 获取要弃掉的卡
-            Card cardToDiscard = currentPlayer.GetSelectedCard(); 
-            if (cardToDiscard != null)
-            {
-                currentPlayer.DiscardCard(cardToDiscard);
-            }
-
-            GameManager.Instance.EndTurn();
-            return;
-        }
-
-        // 选择一名玩家
-        if(currentPlayer.isHuman == true){
-        UIManager.Instance.ShowPlayerSelectionSimple(targetPlayers, selectedPlayer =>
-        {
-            UIManager.Instance.Log($"{currentPlayer.playerName} 查看了 {selectedPlayer.playerName} 的手牌");
-
-            List<Card> cards = selectedPlayer.GetCards();
-
-            if (cards.Count > 0)
-            {
-                Card card = cards[0];
-
-                // 展示卡牌图像
-                UIManager.Instance.ShowCardReveal(card, selectedPlayer.playerName, () =>
-                {
-                    GameManager.Instance.EndTurn(); // 展示后结束回合
-                });
-            }
-            else
-            {
-                UIManager.Instance.ShowPopup($"{selectedPlayer.playerName} 没有手牌");
-                GameManager.Instance.EndTurn();
-            }
-        });
-        }else{
-            return;
-     
-     };
-    }
-}
->>>>>>> d2b8dbccd4cca094b84f0b0d7fa966467d65b6b1
--- Assets/Scripts/AI_CardEffect/AiCardEffect7.cs
+++ Assets/Scripts/AI_CardEffect/AiCardEffect7.cs
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 using UnityEngine;
 
 public class AiCardEffect7 : MonoBehaviour, IMainEffect
@@ -9,15 +9,4 @@ public class AiCardEffect7 : MonoBehaviour, IMainEffect
         GameManager.Instance.EndTurn();
     }
 }
-=======
-using UnityEngine;
 
-public class AiCardEffect7 : MonoBehaviour, IMainEffect
-{
-    public void ExecuteEffect(Player currentPlayer)
-    {
-        Debug.Log($"{currentPlayer.playerName} 打出了7号牌，没有任何效果");
-        GameManager.Instance.EndTurn();
-    }
-}
->>>>>>> d2b8dbccd4cca094b84f0b0d7fa966467d65b6b1
